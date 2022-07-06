bcf6f72c6e2e90ea7ee113bb4eb333b84c0072a8
AquaApple <yassershehab9@gmail.com>
2022-07-06 13:28:10 +0200

    Accordion Project Completed

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ id, title, info }) => {
+  const [isDisplayed, setIsDisplayed] = useState(false);
+
+  const handleDisplay = () => {
+    setIsDisplayed(!isDisplayed);
+  };
+
+  return (
+    <article className="question">
+      <header>
+        <h4>{title}</h4>
+        <button className="btn" onClick={handleDisplay}>
+          {isDisplayed ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {isDisplayed && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
